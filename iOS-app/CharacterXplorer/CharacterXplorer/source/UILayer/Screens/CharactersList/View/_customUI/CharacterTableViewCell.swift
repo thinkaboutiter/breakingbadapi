fbebc2dd77017d82b18b344103c982c8bbe96e2b
boyankov <boyan.yankov@yahoo.com>
2020-02-16 01:43:23 +0200

    bba-3: added `CharacterTableViewCell`

---
+++ iOS-app/CharacterXplorer/CharacterXplorer/source/UILayer/Screens/CharactersList/View/_customUI/CharacterTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterTableViewCell.swift
+//  CharacterXplorer
+//
+//  Created by Boyan Yankov on 2020-W07-16-Feb-Sun.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: BaseTableViewCell {
+    
+    // MARK: - Properties
+    @IBOutlet private weak var avatarImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var niknameLabel: UILabel!
+    
+    // MARK: - Life cycle
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        // TODO: stop any image requests
+    }
+    
+    // MARK: - Configurations
+    func configure(with character: BreakingBadCharacter) {
+        self.nameLabel.text = character.name
+        self.niknameLabel.text = character.nickname
+    }
+}
