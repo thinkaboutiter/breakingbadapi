b40900af7dc36233651bde15be515c01c16f7973
boyankov <boyan.yankov@yahoo.com>
2020-02-15 17:08:23 +0200

    bba-3: added `BreakingBadCharacter.swift`

---
+++ iOS-app/CharacterXplorer/CharacterXplorer/source/DataLayer/Model/BreakingBadCharacter.swift
@@ -0,0 +1,60 @@
+//
+//  BreakingBadCharacter.swift
+//  CharacterXplorer
+//
+//  Created by Boyan Yankov on 2020-W07-15-Feb-Sat.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import Foundation
+
+protocol BreakingBadCharacter {
+    var charID: Int { get }
+    var name: String { get }
+    var imageUrlString: String { get }
+    var nickname: String { get }
+    var seasonAppearance: [Int] { get }
+}
+
+struct BreakingBadCharacterAppEntity: BreakingBadCharacter {
+    
+    // MARK: - Properties
+    let charID: Int
+    let name: String
+    let imageUrlString: String
+    let nickname: String
+    let seasonAppearance: [Int]
+    
+    // MARK: - Initialization
+    init(webEntity: BreakingBadCharacterWebEntity) {
+        self.charID = webEntity.charID
+        self.name = webEntity.name
+        self.imageUrlString = webEntity.imageUrlString
+        self.nickname = webEntity.nickname
+        self.seasonAppearance = webEntity.appearance
+    }
+}
+
+struct BreakingBadCharacterWebEntity: Codable {
+    
+    // MARK: - Properties
+    let charID: Int
+    let name: String
+    let birthday: String
+    let occupation: [String]
+    let imageUrlString: String
+    let status: String
+    let nickname: String
+    let appearance: [Int]
+    let portrayed: String
+    let category: String
+    let betterCallSaulAppearance: [Int]
+    
+    // MARK: - Codable protocol
+    enum CodingKeys: String, CodingKey {
+        case charID = "char_id"
+        case name, birthday, occupation, status, nickname, appearance, portrayed, category
+        case imageUrlString = "img"
+        case betterCallSaulAppearance = "better_call_saul_appearance"
+    }
+}
