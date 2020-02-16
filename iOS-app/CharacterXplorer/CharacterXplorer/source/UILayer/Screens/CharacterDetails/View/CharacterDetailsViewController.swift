5170133885750363e4ef9a3781767ffd367911b1
boyankov <boyan.yankov@yahoo.com>
2020-02-17 01:23:46 +0200

    bba-3: updated `CharacterDetailsViewController` - added ui configurations

--- iOS-app/CharacterXplorer/CharacterXplorer/source/UILayer/Screens/CharacterDetails/View/CharacterDetailsViewController.swift
+++ iOS-app/CharacterXplorer/CharacterXplorer/source/UILayer/Screens/CharacterDetails/View/CharacterDetailsViewController.swift
@@ -18,6 +18,18 @@ class CharacterDetailsViewController: BaseViewController, CharacterDetailsViewMo
     
     // MARK: - Properties
     private let viewModel: CharacterDetailsViewModel
+    private var imageCacheManager: ImageCacheManager {
+        return ImageCacheManagerImpl.shared
+    }
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var occupationTitleLabel: UILabel!
+    @IBOutlet weak var occupationLabel: UILabel!
+    @IBOutlet weak var statusTitleLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var seasonAppearanceTitleLabel: UILabel!
+    @IBOutlet weak var seasonAppearanceLabel: UILabel!
     
     // MARK: - Initialization
     @available(*, unavailable, message: "Creating this view controller with `init(coder:)` is unsupported in favor of initializer dependency injection.")
@@ -26,7 +38,9 @@ class CharacterDetailsViewController: BaseViewController, CharacterDetailsViewMo
     }
     
     @available(*, unavailable, message: "Creating this view controller with `init(nibName:bundle:)` is unsupported in favor of initializer dependency injection.")
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    override init(nibName nibNameOrNil: String?,
+                  bundle nibBundleOrNil: Bundle?)
+    {
         fatalError("Creating this view controller with `init(nibName:bundle:)` is unsupported in favor of dependency injection initializer.")
     }
     
@@ -47,7 +61,36 @@ class CharacterDetailsViewController: BaseViewController, CharacterDetailsViewMo
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        let character: BreakingBadCharacter = self.viewModel.getCharacter()
+        self.configure_ui(with: character)
+    }
+}
+
+// MARK: - Configurations
+private extension CharacterDetailsViewController {
+    
+    func configure_ui(with character: BreakingBadCharacter) {
+        self.avatarImageView.configure(with: character.imageUrlString,
+                                       using: self.imageCacheManager)
+        self.nameLabel.text = character.name
+        self.nicknameLabel.text = character.nickname
+        let occupationTitle: String =
+            NSLocalizedString("CharacterDetailsViewController.occupationTitle.occupation",
+                              comment: AppConstants.LocalizedStringComment.labelTitle)
+        self.occupationTitleLabel.text = "\(occupationTitle):"
+        self.occupationLabel.text = character.occupation
+            .joined(separator: ", ")
+        let statusTitle: String =
+            NSLocalizedString("CharacterDetailsViewController.statusTitle.status",
+                              comment: AppConstants.LocalizedStringComment.labelTitle)
+        self.statusTitleLabel.text = "\(statusTitle):"
+        self.statusLabel.text = character.status
+        let seasonAppearanceTitle: String =
+            NSLocalizedString("CharacterDetailsViewController.seasonAppearanceTitle.season-appearance",
+                              comment: AppConstants.LocalizedStringComment.labelTitle)
+        self.seasonAppearanceTitleLabel.text = "\(seasonAppearanceTitle):"
+        self.seasonAppearanceLabel.text = character.seasonAppearance
+            .map() { "\($0)" }
+            .joined(separator: ", ")
     }
 }
