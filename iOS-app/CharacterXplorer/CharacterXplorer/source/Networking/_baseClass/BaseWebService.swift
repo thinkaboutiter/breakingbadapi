32abb1d4672784798031104f96ab0ba7415360c0
boyankov <boyan.yankov@yahoo.com>
2020-02-18 00:50:50 +0200

    bba-4: updated `BaseWebService.swift` - made generic - added base implmentations

--- iOS-app/CharacterXplorer/CharacterXplorer/source/Networking/_baseClass/BaseWebService.swift
+++ iOS-app/CharacterXplorer/CharacterXplorer/source/Networking/_baseClass/BaseWebService.swift
@@ -7,8 +7,10 @@
 //
 
 import Foundation
+import SimpleLogger
 import Alamofire
 
+/// WebService protocol.
 protocol WebService {
     
     /// Base endpoint for all services.
@@ -26,6 +28,9 @@ protocol WebService {
     /// Request headers.
     func requestHeaders() -> [String: String]?
     
+    /// Add parameters to the request.
+    func requestParameters() -> Parameters?
+    
     /// Request parameters encoding.
     func requestParametersEncoding() -> Alamofire.ParameterEncoding
     
@@ -35,25 +40,25 @@ protocol WebService {
 
 /// Abstract.
 /// Subclass to create web service object.
-class BaseWebService: WebService {
+class BaseWebService<T: Decodable>: WebService {
     
     // MARK: - Properties
-    let endpoint: Endpoint
+    let endpoint: WebServiceConstants.Endpoint
     var request: Alamofire.Request?
     
     // MARK: - Initialization
-    init(endpoint: Endpoint) {
+    init(endpoint: WebServiceConstants.Endpoint) {
         self.endpoint = endpoint
     }
     
     // MARK: - WebService protocol
     static func baseEndpoint() -> String {
-        let result: String = "\(HttpPrefix.secure)\(Constants.domain)"
+        let result: String = "\(WebServiceConstants.HttpPrefix.secure)\(WebServiceConstants.domain)"
         return result
     }
     
     func instanceEndpoint() -> String {
-        return ""
+        return self.endpoint.rawValue
     }
     
     func serviceEndpoint() -> String {
@@ -71,12 +76,57 @@ class BaseWebService: WebService {
         return nil
     }
     
+    func requestParameters() -> Parameters? {
+        return nil
+    }
+    
     func requestParametersEncoding() -> ParameterEncoding {
         return URLEncoding.default
     }
     
     func responseJSONReadingOptions() -> JSONSerialization.ReadingOptions {
         return JSONSerialization.ReadingOptions.mutableContainers
+    }
+    
+    // MARK: - Fetching
+    func fetch(success: @escaping (_ responseObject: T) -> Void,
+               failure: @escaping (_ error: NSError) -> Void)
+    {
+        self.request?.cancel()
+        self.request = Alamofire
+            .request(
+                self.serviceEndpoint(),
+                method: self.httpVerb(),
+                parameters: self.requestParameters(),
+                encoding: self.requestParametersEncoding(),
+                headers: self.requestHeaders())
+            .responseData(completionHandler: { (response: DataResponse<Data>) in
+                Logger.network.message("request:").object(response.request)
+                Logger.network.message("request.allHTTPHeaderFields:").object(response.request?.allHTTPHeaderFields)
+                Logger.network.message("response:").object(response.response)
+                Logger.network.message("timeline:").object(response.timeline)
+                do {
+                    try self.validateResponse(response)
+                    
+                    guard let valid_responseData: Data = response.result.value else {
+                        let message: String = NSLocalizedString("Unable to obtain response object!",
+                                                                comment: AppConstants.LocalizedStringComment.errorMessage)
+                        let error: NSError = ErrorCreator
+                            .custom(domain: BaseWebServiceError.domainName,
+                                    code: BaseWebServiceError.Code.unableToObtainResponseObject,
+                                    localizedMessage: message)
+                            .error()
+                        failure(error)
+                        return
+                    }
+                    let decoder: JSONDecoder = JSONDecoder()
+                    let result: T = try decoder.decode(T.self, from: valid_responseData)
+                    success(result)
+                }
+                catch {
+                    failure(error as NSError)
+                }
+            })
     }
     
     // MARK: - Validation
@@ -91,8 +141,8 @@ class BaseWebService: WebService {
             let message: String = NSLocalizedString("Invalid response object",
                                                     comment: AppConstants.LocalizedStringComment.errorMessage)
             let error: NSError = ErrorCreator
-                .custom(domain: InternalError.domainName,
-                        code: InternalError.Code.invalidResponseObject,
+                .custom(domain: BaseWebServiceError.domainName,
+                        code: BaseWebServiceError.Code.invalidResponseObject,
                         localizedMessage: message)
                 .error()
             throw error
@@ -103,8 +153,8 @@ class BaseWebService: WebService {
             let message: String = NSLocalizedString("Invalid status code=\(valid_response.statusCode)",
                                                     comment: AppConstants.LocalizedStringComment.errorMessage)
             let error: NSError = ErrorCreator
-                .custom(domain: InternalError.domainName,
-                        code: InternalError.Code.invalidStatusCode,
+                .custom(domain: BaseWebServiceError.domainName,
+                        code: BaseWebServiceError.Code.invalidStatusCode,
                         localizedMessage: message)
                 .error()
             throw error
@@ -113,10 +163,9 @@ class BaseWebService: WebService {
 }
 
 // MARK: - Constants
-extension BaseWebService {
-    enum Constants {
-        static let domain: String = "breakingbadapi.com"
-    }
+enum WebServiceConstants {
+    
+    static let domain: String = "breakingbadapi.com"
     
     enum HttpPrefix {
         static let secure: String = "https://"
@@ -128,14 +177,12 @@ extension BaseWebService {
 }
 
 // MARK: - Errors
-private extension BaseWebService {
+private enum BaseWebServiceError {
+    static let domainName: String = "\(AppConstants.projectName).\(String(describing: BaseWebServiceError.self))"
     
-    enum InternalError {
-        static let domainName: String = "\(AppConstants.projectName).\(String(describing: BaseWebService.self)).\(String(describing: InternalError.self))"
-        
-        enum Code {
-            static let invalidResponseObject: Int = 9000
-            static let invalidStatusCode: Int = 9001
-        }
+    enum Code {
+        static let invalidResponseObject: Int = 9000
+        static let invalidStatusCode: Int = 9001
+        static let unableToObtainResponseObject: Int = 9002
     }
 }
